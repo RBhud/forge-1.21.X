a2640eae5378f257db37f32d52ae3895f976b7e4
Rushil Bhudia <zlac200@live.rhul.ac.uk>
2025-06-10 14:28:03 +0100

    Added two new mod items.

---
+++ src/main/java/net/rushil/tutorialmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.rushil.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rushil.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> GEMSTONE = ITEMS.register("gemstone",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_GEMSTONE = ITEMS.register("raw_gemstone",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
